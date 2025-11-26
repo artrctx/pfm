37906ef107a45deba6d30e751467c0f3f359127d
arthurDiff <arthun.ctx@gmail.com>
2025-11-25 19:42:48 -0600

    add stun client

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+
+	"github.com/arthurDiff/pfm/internal/stun"
 	"github.com/spf13/cobra"
 )
 
@@ -9,7 +12,30 @@ var rootCmd = &cobra.Command{
 	Use:   "pfm",
 	Short: "Port forward designated port and make it accesible",
 	Long: `Port forward given port so your friend can access your destination.
-	pfm --help`,
+	pfm --stun stun:stun1.l.google.com:3478`,
+	Run: portForwardMe,
+}
+
+func portForwardMe(cmd *cobra.Command, args []string) {
+	flags := cmd.Flags()
+	stunAddr, err := flags.GetString("stun")
+	if err != nil {
+		log.Fatalf("Failed to get stun flag value error %v", err)
+	}
+	stunClient := stun.NewClient(stunAddr)
+	defer stunClient.Close()
+
+	// TODO
+	// CONFIG FIREWALL TO PROVIDED OPEN PORT
+	// IN FOR LOOP
+	// - SHOULD GET CURRENTLY CONFIGURED DNS IP
+	// - GET ACTUAL IP
+	// -- IF MISMATCH UPDATE CONFIG
+	// MAYBE PANIC NOTIFIER
+}
+
+func init() {
+	rootCmd.Flags().StringP("stun", "s", "stun:stun1.l.google.com:3478", "STUN address to use. (Default: stun:stun1.l.google.com:3478)")
 }
 
 func main() {
--- internal/stun/stun.go
+++ internal/stun/stun.go
@@ -1,14 +1,57 @@
 package stun
 
-import "net/http"
+import (
+	"log"
 
-type STUNClient http.Client
+	stn "github.com/pion/stun"
+)
 
-var STUN *STUNClient
+// https://dev.to/alakkadshaw/google-stun-server-list-21n4
+// https://datatracker.ietf.org/doc/html/rfc5389
+type Client struct{ client *stn.Client }
 
-// func NewSTUNClient() *STUNClient {
-// 	if STUN != nil {
-// 		return STUN
-// 	}
-// 	// todo
-// }
+var client *Client
+
+func NewClient(stunAddr string) *Client {
+	if client != nil {
+		return client
+	}
+
+	// stunAddr := "stun:stun1.l.google.com:3478"
+	addr, err := stn.ParseURI(stunAddr)
+	if err != nil {
+		log.Panicf("tcp addr resolve error: %v", err)
+	}
+
+	c, err := stn.DialURI(addr, &stn.DialConfig{})
+	if err != nil {
+		log.Panicf("tcp conn error: %v", err)
+	}
+
+	client = &Client{c}
+	return client
+}
+
+func (c *Client) GetIP() (stn.XORMappedAddress, error) {
+	var err error
+	var xorAddr stn.XORMappedAddress
+	if err = c.client.Do(stn.MustBuild(stn.TransactionID, stn.BindingRequest), func(res stn.Event) {
+		//do stuff
+		if res.Error != nil {
+			err = res.Error
+			return
+		}
+
+		if err = xorAddr.GetFrom(res.Message); err != nil {
+			return
+		}
+	}); err != nil {
+		return xorAddr, err
+	}
+
+	return xorAddr, nil
+}
+
+func (c *Client) Close() error {
+	return c.client.Close()
+}
